6295f7ea343273c9ad3c0c706191132fb79f5195
theRingleman <sam.ringleman@gmail.com>
2016-09-03 15:25:05 -0600

    add search feature

--- app/assets/javascripts/angular/controllers/ResourcesCtrl.js
+++ app/assets/javascripts/angular/controllers/ResourcesCtrl.js
@@ -2,6 +2,12 @@ function ResourcesCtrl($scope, Resource, $filter) {
   var self = this;
 
   self.resources = Resource.query();
+
+  self.refilter = function () {
+    self.filteredList = $filter('filter')(self.resources, self.search);
+  };
+
+  self.refilter();
 }
 
 ResourcesCtrl.$inject = ["$scope", "Resource", '$filter'];
