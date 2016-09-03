ffc622430b5d6d377c96f87de661022b8efcda6c
theRingleman <sam.ringleman@gmail.com>
2016-09-02 19:34:14 -0600

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Resource.create(name: "SRL", url: "https://simple-regex.com/", description: "Regular Expressions made simple", upvote: 0, category: "Regex")
+Resource.create(name: "Markdown Cheatsheet", url: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#code", description: "A cheatsheet for markdown.", upvote: 0, category: "Markdown")
+Resource.create(name: "Rails Tutorial", url: "https://www.railstutorial.org/", description: "A tutorial by Michael Hartl to learn Rails", upvote: 0, category: "Ruby on Rails")
+Resource.create(name: "Everyday Rails Testing with RSPEC", url: "https://leanpub.com/everydayrailsrspec", description: "An excellent book to teach RSPEC", upvote: 0, category: "RSPEC")
+Resource.create(name: "Lets Build a Sinatra App", url: "http://matt.weppler.me/2013/07/19/lets-build-a-sinatra-app.html", description: "A tutorial to introduct Sinatra", upvote: 0, category: "Sinatra")
